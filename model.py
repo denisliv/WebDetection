9dd4fcb798e3583c5f7a2a658df074812075e5b6
denisliv <denisliv13@gmail.com>
2023-01-29 22:47:18 +0300

    Initial commit

---
+++ model.py
@@ -0,0 +1,23 @@
+from ultralytics import YOLO
+from collections import Counter
+from settings import CLASSES, ALLOWED_IMAGE_TYPES
+
+class Model:
+    def __init__(self, model='models/yolov8m.pt'):
+        self.model = YOLO(model)
+
+    def predict(self, filename):
+        return self.model.predict(source=f'static/files/{filename}', save=True, project='static', name='results', line_thickness=2)
+
+
+def label_count(results):
+    counter = Counter(results[0].boxes.cls.cpu().numpy())
+    for i in counter.copy():
+        if i in CLASSES:
+            counter[CLASSES[i].capitalize()] = counter.pop(i)
+    return counter
+
+
+def allowed_image(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_IMAGE_TYPES
---
+++ server.py
@@ -0,0 +1,43 @@
+import os
+import cv2
+from flask import Flask, render_template, request, flash
+from model import Model, label_count, allowed_image
+from settings import Config
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+model = Model()
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_predict():
+
+    if request.method == 'POST':
+        image_file = request.files['image']
+
+        if image_file:
+
+            if allowed_image(image_file.filename):
+                flash('Изображение загружено успешно', category='success')
+                image_file.save(os.path.join(app.config['UPLOAD_FOLDER'], image_file.filename))
+                img = cv2.imread(fr'static\files\{image_file.filename}')
+                results = model.predict(image_file.filename)
+                labels = label_count(results)
+
+                return render_template('index.html',
+                                        image_loc=image_file.filename,
+                                        width=min(img.shape[1], 600),
+                                        height=min(img.shape[0], 400),
+                                        labels=labels)
+
+            else:
+                flash('Допустимые расширения: png, jpg, jpeg', category='error')
+                return render_template('index.html', image_loc=None)
+
+    return render_template('index.html', image_loc=None)
+
+
+
+if __name__ == '__main__':
+    app.run()
